53e88fbb925abd9e0801e785e6ce82a2ae96f58f
阆苑 <3174541442@qq.com>
2020-06-14 17:28:57 +0800

    引入自己的初始化样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+// 引入自己写的样式初始化样式
+import './index.css'
 // 引入 antd-mobille 样式
 import 'antd-mobile/dist/antd-mobile.css'
 ReactDOM.render(<App />, document.getElementById('root'))
