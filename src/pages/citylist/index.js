664baa7c899b53b6027f5da4f7d11fedfa81feaa
阆苑 <3174541442@qq.com>
2020-06-17 18:46:04 +0800

    项目-城市选择-navbar 组件渲染

--- src/pages/citylist/index.js
+++ src/pages/citylist/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+// 导入 antd-mobile 组件
+import { NavBar, Icon } from 'antd-mobile'
 export default class CityList extends Component {
+
   render() {
     return (
       <div className="cityList">
-        这是城市选择组件
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => {this.props.history.push('/home/index')}}
+        >城市选择</NavBar>
       </div>
     )
   }
