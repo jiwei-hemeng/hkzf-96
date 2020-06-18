5f4ad90fdae8cb9530c7563aaf39371ce7a8b026
阆苑 <3174541442@qq.com>
2020-06-18 14:38:27 +0800

    项目-cityList组件-将当前定位信息存入本地存储

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,23 @@
 import axios from 'axios'
 // 封装并导出当前定位的城市的数据
 export const getCurrentCity = () => {
-  return new Promise((resolve,reject) => {
-    // 1. 获取当前定位城市
-    const mycity = new window.BMap.LocalCity() 
-    mycity.get(async (res) => {
-      let cityName = res.name
-      console.log(cityName)
-      // 2. 根据城市名字获取当前定位城市
-      const localCity = await axios({
-        url: 'http://api-haoke-dev.itheima.net/area/info?name=' + cityName
+  let city = JSON.parse(localStorage.getItem('my-city'))
+  if(!city){
+    return new Promise((resolve,reject) => {
+      // 1. 获取当前定位城市
+      const mycity = new window.BMap.LocalCity() 
+      mycity.get(async (res) => {
+        let cityName = res.name
+        console.log(cityName)
+        // 2. 根据城市名字获取当前定位城市
+        const localCity = await axios({
+          url: 'http://api-haoke-dev.itheima.net/area/info?name=' + cityName
+        })
+        localStorage.setItem('my-city', JSON.stringify(localCity.data.body))
+        resolve(localCity.data.body)
       })
-      resolve(localCity.data.body)
     })
-  })
+  }
+  // Promise 的简写形式
+  return Promise.resolve(city)
 }
