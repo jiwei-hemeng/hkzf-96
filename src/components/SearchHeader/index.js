5fd85b31d18c01ae1e1a8d42b9ac910fee5d2f29
阆苑 <3174541442@qq.com>
2020-06-23 10:04:12 +0800

    项目-公用组件SearchHeader 的封装

---
+++ src/components/SearchHeader/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { withRouter} from 'react-router-dom'
+import { Flex } from 'antd-mobile'
+import PropTypes from 'prop-types'
+class SearchHeader extends Component {
+  render() {
+    return (
+      <Flex className='searchBox'>
+          <Flex className='searchLeft'>
+            <div
+              className='location'
+              onClick={() => {
+                this.props.history.push('/citylist')
+              }}
+            >
+            <span>{this.props.cityname}</span>
+            <i className="iconfont icon-below-s" />
+            </div>
+            <div
+            className='searchForm'
+            >
+              <i className="iconfont icon-search1" />
+              <span>请输入小区或地址</span>
+            </div>
+          </Flex>
+          <i className="iconfont icon-map" onClick={() => {
+            this.props.history.push('/map')
+          }}  />
+        </Flex>
+    )
+  }
+}
+
+// 校验数据类型
+SearchHeader.propsTypes = {
+  cityname: PropTypes.string
+}
+
+// 设置传参默认值
+SearchHeader.defaultProps={
+  cityname: '全国'
+}
+
+export default withRouter(SearchHeader)
--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Filter from './components/Filter'
+import SearchHeader from '../../components/SearchHeader'
 export default class HouseList extends React.Component{
   render () {
     return (
       <div>
-        <Filter />
+        <SearchHeader></SearchHeader>
+        {/* <Filter /> */}
       </div>
     )
   }
