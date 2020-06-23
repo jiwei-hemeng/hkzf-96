044f80861a6c5f940261cce1f9671a973b6da70f
阆苑 <3174541442@qq.com>
2020-06-23 11:49:10 +0800

    项目-cityList组件给SearchHeader传递定位城市信息

--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Filter from './components/Filter'
 import SearchHeader from '../../components/SearchHeader'
+import { getCurrentCity } from '../../utils/index'
 import './index.scss'
 export default class HouseList extends React.Component{
+  state = {
+    cityname: ''
+  }
+  componentDidMount(){
+    this.getCityName()
+  }
+  async getCityName(){
+    let res = await getCurrentCity()
+    console.log(res)
+    this.setState({
+      cityname: res.label
+    })
+  }
   render () {
     return (
       <div className="houselist">
         <div className="Header">
           <i className="iconfont icon-jiantouarrowhead7"></i>
-          <SearchHeader className="searchhearder"></SearchHeader>
+          <SearchHeader 
+            className="searchhearder"
+            cityname={ this.state.cityname }
+          ></SearchHeader>
         </div>
         <Filter />
       </div>
