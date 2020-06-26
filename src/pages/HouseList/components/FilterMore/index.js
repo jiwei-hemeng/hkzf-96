f635bd2a42865880cb44b829da679d44b4833523
阆苑 <3174541442@qq.com>
2020-06-26 11:25:44 +0800

    项目-HouList模块-filterMore组件渲染得到的数据

--- src/pages/HouseList/components/FilterMore/index.js
+++ src/pages/HouseList/components/FilterMore/index.js
@@ -6,14 +6,24 @@ import styles from './index.module.css'
 
 export default class FilterMore extends Component {
   // 渲染标签
-  renderFilters() {
+  renderFilters(arr) {
     // 高亮类名： styles.tagActive
-    return (
-      <span className={[styles.tag, styles.tagActive].join(' ')}>东北</span>
-    )
+
+    // return (
+    //   <span className={[styles.tag, styles.tagActive].join(' ')}>东北</span>
+    // )
+    return arr.map((item, index)=>{
+      return (
+        <span
+          key={item.value}
+          className={[styles.tag, ''].join(' ')}
+        >{item.label}</span>
+      )
+    })
   }
 
   render() {
+    const { data } = this.props
     return (
       <div className={styles.root}>
         {/* 遮罩层 */}
@@ -23,16 +33,16 @@ export default class FilterMore extends Component {
         <div className={styles.tags}>
           <dl className={styles.dl}>
             <dt className={styles.dt}>户型</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(data.roomType)}</dd>
 
             <dt className={styles.dt}>朝向</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(data.oriented)}</dd>
 
             <dt className={styles.dt}>楼层</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(data.floor)}</dd>
 
             <dt className={styles.dt}>房屋亮点</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(data.characteristic)}</dd>
           </dl>
         </div>
 
