3b7e21bb1faf8f040bb3f50d74474a3abbda788b
阆苑 <3174541442@qq.com>
2020-06-27 17:12:57 +0800

    项目-创建房源详情页

---
+++ src/pages/HouseDetail/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div className="housesetail">
+        房源详情页
+      </div>
+    )
+  }
+}
